04bb6c97492767a5797ba7139f1aca1b71c31c31
Beat Furrer (fub) <beat.furrer@sisag.ch>
2025-03-04 11:54:19 +0100

    create api for front end with flusk

---
+++ app/windseekerapi.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, jsonify
+import database as db
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "<p>Hello world!<p>"
+
+@app.route('/users', methods=['GET'])
+def get_users_all():
+    client = db.connect_to_db()
+    users = db.find_all(client)
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run()
